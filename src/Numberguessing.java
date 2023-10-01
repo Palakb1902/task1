238ebcf6b56ad2d5dec8a1ac32944d1547222f3d
Palak Barapatre <78619525+Palakb1902@users.noreply.github.com>
2023-10-01 17:06:14 +0530

    Update Numberguessing.java

--- src/Numberguessing.java
+++ src/Numberguessing.java
@@ -1,45 +1,54 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Numberguessing {
+class game {
+    Random rand = new Random();
+    private int compNo, inp;
+    private int noOf = 10;
 
-    public static void game(){
-        Scanner sc = new Scanner(System.in);
-        int i,guess;
-        int generate = 1+ (int)(100* Math.random());
-        int m = 3;
+     game() {
+        compNo = rand.nextInt(100);
+    }
 
-        System.out.println(
-                "A number is chosen"
-                        + " between 1 to 100."
-                        + "Guess the number"
-                        + " within 3 trials.");
-        for ( i = 0; i < m; i++) {
-            System.out.println(" guess the number");
-            guess = sc.nextInt();
-            if(generate==guess){
-                System.out.println("Congrats you guesses the correct number");
-                break;
-            } else if (generate > guess  && i != m - 1) {
-                System.out.println(" guess a larger number");
+     void takeUserInput(int inpu) {
+        this.inp = inpu;
+    }
 
-            } else if (generate < guess && i != m - 1) {
-                System.out.println(" guess a smaller number");
+     String isCorrectNumber() {
+        return "your choice is correct";
+    }
 
-            }
+     void noOfGuesses() {
 
+        if (inp == compNo) {
+            System.out.println(isCorrectNumber());
+            System.out.println("Score is: "+noOf+"/10");
+        } else if (inp > compNo) {
+            System.out.println("Your guess is greater than expected No");
+            System.out.println("chances left: " +(noOf - 1));
+            noOf--;
+        } else {
+            System.out.println("Your guess is less than expected no.");
+            System.out.println("Chances left: "+ (noOf - 1));
+            noOf--;
         }
-        if (m==i){
-            System.out.println(" you have exhausted your attempts");
-        }
-
-
 
     }
+
+}
+
+ class NoOfGuess {
     public static void main(String[] args) {
-        game();
+        Scanner sc = new Scanner(System.in);
+        game g = new game();
+        System.out.println("lets play a game ,  GUESS THE NUMBER");
+        System.out.println("you will get 10 chances to guess the number ");
+        for (int i = 0; i < 10; i++) {
+            int choice = sc.nextInt();
+            g.takeUserInput(choice);
+            g.noOfGuesses();
 
-
-
-
+        }
     }
+
 }
